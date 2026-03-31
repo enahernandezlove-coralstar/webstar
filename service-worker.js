572f959105927e564de1e7436a6d52f4e209cab2
enahernandezlove-coralstar <enahernandez.love@gmail.com>
2026-03-30 21:40:55 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+﻿const cacheName = 'duckduckgo-pwa-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
